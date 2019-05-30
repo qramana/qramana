81126051d3d1e665e8a1186b220b17e8cc2fdfa3
kadora <k.kadora@gmail.com>
2019-05-30 19:32:51 +0900

    update jsqubits.d.ts

--- typings/jsqubits.d.ts
+++ typings/jsqubits.d.ts
@@ -4,21 +4,32 @@ declare namespace jsqubits {
   namespace jsqubits {
     interface QState {
       multiply(amount: number | Complex): QState;
-      tensorProduct(qstate: QState): QState;
-      X(targetBits: number): QState;
-      Y(targetBits: number): QState;
-      Z(targetBits: number): QState;
-      r(targetBits: number, angle: number): QState;
-      s(targetBits: number): QState;
-      t(targetBits: number): QState;
-      hadamard(targetBits: number): QState;
+      tensorProduct(qState: QState): QState;
+      x(targetBits: SingleQubitOperatorTargetQubits): QState;
+      X(targetBits: SingleQubitOperatorTargetQubits): QState;
+      not(targetBits: SingleQubitOperatorTargetQubits): QState;
+      y(targetBits: SingleQubitOperatorTargetQubits): QState;
+      Y(targetBits: SingleQubitOperatorTargetQubits): QState;
+      z(targetBits: SingleQubitOperatorTargetQubits): QState;
+      Z(targetBits: SingleQubitOperatorTargetQubits): QState;
+      hadamard(targetBits: SingleQubitOperatorTargetQubits): QState;
+      r(targetBits: SingleQubitOperatorTargetQubits, angle: number): QState;
+      s(targetBits: SingleQubitOperatorTargetQubits): QState;
+      S(targetBits: SingleQubitOperatorTargetQubits): QState;
+      t(targetBits: SingleQubitOperatorTargetQubits): QState;
+      T(targetBits: SingleQubitOperatorTargetQubits): QState;
       cnot(controlBits: number, targetBits: number): QState;
-      rotateX(targetBits: number, angle: number): QState;
-      rotateY(targetBits: number, angle: number): QState;
-      rotateZ(targetBits: number, angle: number): QState;
+      rotateX(targetBits: SingleQubitOperatorTargetQubits, angle: number): QState;
+      rotateY(targetBits: SingleQubitOperatorTargetQubits, angle: number): QState;
+      rotateZ(targetBits: SingleQubitOperatorTargetQubits, angle: number): QState;
+      measure(bits: number | number[] | jsqubits.ALL | BitsRange): Measurement;
+
       toString(): string;
       numBits(): number;
-      measure(bits: number | number[] | jsqubits.ALL | MeasureBitsRange): Measurement;
+
+      add(qState: QState): QState;
+      subtract(qState: QState): QState;
+      normalize(): QState;
     }
 
     export type ALL = "ALL";
@@ -52,15 +63,19 @@ interface JSQubitsStatic {
 }
 
 interface InternalJSQubitsStatic {
-  (bitString: string): any;
+  (bitString: string): jsqubits.jsqubits.QState;
   QState: QStateStatic;
   Complex: ComplexStatic;
   Measurement: MeasurementStatic;
-  real: (real: number) => ComplexStatic;
+  real: (real: number) => jsqubits.jsqubits.Complex;
+  complex: (real: number, imaginary: number) => jsqubits.jsqubits.Complex;
+  ZERO: ComplexStatic["ZERO"];
+  ONE: jsqubits.jsqubits.Complex;
+  ALL: string;
 }
 
 interface QStateStatic {
-  new (bitString: string): jsqubits.jsqubits.QState;
+  new (numBits: number, amplitudes?: jsqubits.jsqubits.Complex[]): jsqubits.jsqubits.QState;
   fromBits(bitString: string): jsqubits.jsqubits.QState;
 }
 
@@ -72,14 +87,11 @@ interface ComplexStatic {
   new (real: number, imaginary: number): jsqubits.jsqubits.Complex;
 }
 
-/*
-interface MeasurementStatic {
-  new (numBits: number, result: number, newState: jsqubits.jsqubits.QState): jsqubits.jsqubits.Measurement;
-}
-*/
 type MeasurementStatic = new (numBits: number, result: number, newState: jsqubits.jsqubits.QState) => jsqubits.jsqubits.Measurement;
 
-interface MeasureBitsRange {
+type SingleQubitOperatorTargetQubits = number | number[] | InternalJSQubitsStatic["ALL"] | BitsRange;
+
+interface BitsRange {
   from: number;
   to: number;
 }
