07f23498a8553f50a45d81add4c2e0f53f52973c
dasoran <dasoran@gmail.com>
2019-06-02 16:16:24 +0900

    add y-gate tests and modify test names

--- spec/src/QubitSpec.ts
+++ spec/src/QubitSpec.ts
@@ -28,13 +28,33 @@ describe("test Qubit", () => {
         done();
     });
 
-    it("x_h", (done: any) => {
+    it("x_in_minus_ket", (done: any) => {
         const qubit = new q.Qubit({ value: "|0>" });
         qubit.x();
         qubit.h();
         expect(qubit.toString()).toBe("(0.7071)|0> + (-0.7071)|1>"); // |->
         qubit.x();
         expect(qubit.toString()).toBe("(-0.7071)|0> + (0.7071)|1>"); // X|-> = -|->
+        done();
+    });
+
+    it("y", (done: any) => {
+        const qubit = new q.Qubit({ value: "|0>" });
+        qubit.y();
+        expect(qubit.toString()).toBe("(i)|1>");
+        qubit.y();
+        expect(qubit.toString()).toBe("|0>");
+        done();
+    });
+
+    it("y_with_1_state", (done: any) => {
+        const qubit = new q.Qubit({ value: "|0>" });
+        qubit.x();
+        expect(qubit.toString()).toBe("|1>");
+        qubit.y();
+        expect(qubit.toString()).toBe("(-i)|0>");
+        qubit.y();
+        expect(qubit.toString()).toBe("|1>");
         done();
     });
 });
