251c1e04f884a48a232e8038b6cd410f3de0e6ce
kamakiri01 <kamakiri01@users.noreply.github.com>
2019-06-02 19:14:17 +0900

    add QuantumStateJsqubitsSpec.ts

---
+++ spec/src/QuantumStateJsqubitsSpec.ts
@@ -0,0 +1,37 @@
+import * as q from "../../lib/index";
+
+describe("test QuantumStateJsQubits", () => {
+    it("new", (done: any) => {
+        let state = new q.QuantumStateJsqubits("|0>");
+        expect(state.toString()).toBe("|0>");
+        state = new q.QuantumStateJsqubits("|1>");
+        expect(state.toString()).toBe("|1>");
+        done();
+    });
+
+    describe("#toString", () => {
+        it("return jsqubit tostring", (done: any) => {
+            const state = new q.QuantumStateJsqubits("|0>");
+            expect(state.toString()).toBe("|0>");
+            done();
+        });
+    });
+
+    describe("#x", () => {
+        it("applies X operator to 0-ket", (done: any) => {
+            const state = new q.QuantumStateJsqubits("|0>");
+            expect(state.toString()).toBe("|0>");
+            state.x(0);
+            expect(state.toString()).toBe("|1>");
+            done();
+        });
+
+        it("applies X operator to 1-ket", (done: any) => {
+            const state = new q.QuantumStateJsqubits("|1>");
+            expect(state.toString()).toBe("|1>");
+            state.x(0);
+            expect(state.toString()).toBe("|0>");
+            done();
+        });
+    });
+});
