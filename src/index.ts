003e0fa9ca45c65106f6276e0c80a68464a494ba
kadora <k.kadora@gmail.com>
2019-10-03 09:45:46 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ import { Qubit } from "./Qubit";
 export { Qubit };
 import { Core, CoreConfig } from "./Core";
 export { Core, CoreConfig };
-import { intToQubits } from "./qlib/common";
-export { intToQubits };
+import { Util } from "./Util";
+export { Util };
 
 /**
  * Plamanaの挙動を変更する初期化パラメータ
